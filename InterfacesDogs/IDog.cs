578699a5585d73d645decf1b2f1c6094405ade8d
matt-r-mck <matt.r.mck@gmail.com>
2020-07-12 20:56:57 -0400

    Add project files.

---
+++ InterfacesDogs/IDog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesDogs {
+    interface IDog {
+
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string BarkSound { get; set; }
+
+        public void Bark(); //signature for instance method
+
+
+    }
+}
---
+++ InterfacesDogs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InterfacesDogs {
+    class Program {
+        static void Main(string[] args) {
+
+            var col = new Collie {
+                Name = "Lassie", Color = "Light Brown", BarkSound = "Woof!"
+            };
+            col.Bark();
+
+            var box = new Boxer {
+                Name = "Rocky", Color = "Black", BarkSound = "Bark!"
+            };
+            box.Bark();
+
+            var chi = new Chihuahua {
+                Name = "Killer", Color = "White", BarkSound = "Yip yip!"
+            };
+            chi.Bark();
+
+            IDog [] dogs = new IDog [10];
+            dogs[0] = col;
+            dogs[1] = box;
+            dogs[3] = chi;
+
+            foreach(var dog in dogs) {
+                if (dog == null) continue;
+                Console.WriteLine($"A dog named {dog.Name} is {dog.Color}");
+                dog.Bark();
+            }
+
+        }
+    }
+}
